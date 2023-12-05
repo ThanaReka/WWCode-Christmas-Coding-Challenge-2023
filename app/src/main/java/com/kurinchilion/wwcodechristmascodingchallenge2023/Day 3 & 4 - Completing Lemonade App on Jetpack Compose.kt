a1d0dc44fc241272023874fdf68ca71931130233
ThanaReka <r_thanabalan@yahoo.ca>
2023-12-04 20:33:02 -0800

    Completed Lemonade App for Day 4 of Coding Challenge

--- app/src/main/java/com/kurinchilion/wwcodechristmascodingchallenge2023/Day 3 & 4 - Completing Lemonade App on Jetpack Compose.kt
+++ app/src/main/java/com/kurinchilion/wwcodechristmascodingchallenge2023/Day 3 & 4 - Completing Lemonade App on Jetpack Compose.kt
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LemonadeApp() 
+                    LemonadeApp()
                 }
             }
         }
