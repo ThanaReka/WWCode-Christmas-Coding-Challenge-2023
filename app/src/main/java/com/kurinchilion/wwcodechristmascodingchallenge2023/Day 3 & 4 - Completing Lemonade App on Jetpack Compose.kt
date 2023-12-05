03a91cbf082c262e382976cb5df7d9f935a82e4e
ThanaReka <r_thanabalan@yahoo.ca>
2023-12-04 20:33:29 -0800

    Completed Lemonade App for Day 4 of Coding Challenge

--- app/src/main/java/com/kurinchilion/wwcodechristmascodingchallenge2023/Day 3 & 4 - Completing Lemonade App on Jetpack Compose.kt
+++ app/src/main/java/com/kurinchilion/wwcodechristmascodingchallenge2023/Day 3 & 4 - Completing Lemonade App on Jetpack Compose.kt
@@ -1,15 +1,27 @@
 package com.kurinchilion.wwcodechristmascodingchallenge2023
 
+/*
+Day 4 - Cleaning up code from Day 3 to incorporate a reusable composable for repeated code,
+adding background with border to image
+and adding squeeze logic to prompt user to tap a certain number of times
+(using a random number between 2 - 4)
+ */
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,6 +32,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,72 +63,69 @@ fun LemonApp(modifier: Modifier = Modifier
     .fillMaxSize()
     .wrapContentSize(Alignment.Center)
 ) {
-    var result by remember { mutableStateOf(1) }
+    var currentStep by remember { mutableStateOf(1) }
 
-    when (result) {
-        1 ->     Column(
-            modifier = modifier.clickable { result = 2 },
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(R.drawable.lemon_tree),
-                contentDescription = "1"
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                stringResource(R.string.tree_content_description),
-                fontSize = 18.sp
-            )
-        }
+    var numberOfTaps by remember { mutableStateOf(0) }
 
-        2 -> Column(
-            modifier = modifier.clickable { result = 3 },
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(R.drawable.lemon_squeeze),
-                contentDescription = "2"
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                stringResource(R.string.lemon_content_description),
-                fontSize = 18.sp
-            )
-        }
-        3 -> Column(
-            modifier = modifier.clickable { result = 4 },
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(R.drawable.lemon_drink),
-                contentDescription = "3"
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                stringResource(R.string.drink_content_description),
-                fontSize = 18.sp
-            )
-        }
+    when (currentStep) {
+        1 -> LemonTextAndImage(onClick = {currentStep = 2
+            numberOfTaps = (2..4).random()
+        },
+            R.drawable.lemon_tree,
+            "1",
+            R.string.tree_content_description )
 
-        else -> Column(
-            modifier = modifier.clickable { result = 1 },
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(R.drawable.lemon_restart),
-                contentDescription = "4"
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                stringResource(R.string.start_content_description),
-                fontSize = 18.sp
-            )
-        }
+        2 -> LemonTextAndImage(onClick = {numberOfTaps--
+            if(numberOfTaps == 0){currentStep = 3}},
+            R.drawable.lemon_squeeze,
+            "2",
+            R.string.lemon_content_description )
+
+        3 -> LemonTextAndImage(onClick = {currentStep = 4},
+            R.drawable.lemon_drink,
+            "3",
+            R.string.drink_content_description )
+
+        4 -> LemonTextAndImage(onClick = {currentStep = 1},
+            R.drawable.lemon_restart,
+            "4",
+            R.string.start_content_description )
+
     }
 
 }
 
+@Composable
+fun LemonTextAndImage(onClick: () -> Unit,
+                      relevantImage : Int,
+                      contentDescription :String,
+                      relevantDescription : Int,
+){
 
+    Column(
+        modifier = Modifier.clickable {onClick()},
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = painterResource(relevantImage),
+            contentDescription = contentDescription,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .border(5.dp, colorResource(R.color.border_green))
+                .background(colorResource(R.color.border_green))
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            stringResource(relevantDescription),
+            fontSize = 18.sp
+        )
+
+    }
+
+
+}
 @Preview(showBackground = true)
 @Composable
 fun LemonadeApp() {
