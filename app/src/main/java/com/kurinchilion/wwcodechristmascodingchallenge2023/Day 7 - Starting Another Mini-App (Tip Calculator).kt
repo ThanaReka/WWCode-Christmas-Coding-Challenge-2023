2fad2c2fb85dac996aa00996383984d25ea875d9
ThanaReka <r_thanabalan@yahoo.ca>
2023-12-10 00:02:34 -0800

    Continuing The Tip Calculator App on Day 9 of Coding Challenge

--- app/src/main/java/com/kurinchilion/wwcodechristmascodingchallenge2023/Day 7 - Starting Another Mini-App (Tip Calculator).kt
+++ app/src/main/java/com/kurinchilion/wwcodechristmascodingchallenge2023/Day 7 - Starting Another Mini-App (Tip Calculator).kt
@@ -1,9 +1,10 @@
-package com.kurinchilion.wwcodechristmascodingchallenge2023
+package com.example.tipcalculator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,9 +27,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.tiptime.ui.theme.TipTimeTheme
+import com.example.tipcalculator.ui.theme.TipTimeTheme
+import com.example.tiptime.R
 import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
@@ -48,6 +52,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+    /*
+     * use state hoisting to hoist/extracting, the state from a composable to make it stateless composable
+     * (i.e. a composable that doesn't store its own state. It displays whatever state it's given
+     * as input arguments.
+     */
+
+    var amountInput by remember { mutableStateOf("") }
+
+    val amount = amountInput.toDoubleOrNull()?:0.0
+    val tip = calculateTip(amount)
+
+    /*  This makes EditNumberField stateless by having hoisted the UI state to its ancestor, TipTimeLayout().
+     *  The TipTimeLayout() is the state(amountInput) owner now.
+     */
+
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -62,11 +81,16 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(modifier = Modifier
-            .padding(bottom = 32.dp)
-            .fillMaxWidth())
+        EditNumberField(
+            label = R.string.bill_amount,
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth())
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+//            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -75,16 +99,20 @@ fun TipTimeLayout() {
 
 
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier){
-
-    var amountInput by remember { mutableStateOf("") }
-
-    // Compose keeps track of each composable that reads state value
-    // properties and triggers a recomposition when its value changes.
+fun EditNumberField(
+    @StringRes label: Int,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier){
+    
 
     TextField(
-        value = amountInput,
-        onValueChange = {amountInput = it},
+        value = value,
+        onValueChange = onValueChange,
+//        label = {Text(stringResource(R.string.bill_amount))},
+        label = {Text(stringResource(label))},
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier
     )
 }
@@ -107,3 +135,4 @@ fun TipTimeLayoutPreview() {
         TipTimeLayout()
     }
 }
+
