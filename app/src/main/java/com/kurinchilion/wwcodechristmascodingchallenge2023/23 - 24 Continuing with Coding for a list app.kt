9c98b2d59507a4c04dae09d321b2790b9aa5024b
ThanaReka <r_thanabalan@yahoo.ca>
2023-12-24 23:25:18 -0800

    Continuing With Material Theme For a List App on Day 24 of Coding Challenge

--- app/src/main/java/com/kurinchilion/wwcodechristmascodingchallenge2023/23 - 24 Continuing with Coding for a list app.kt
+++ app/src/main/java/com/kurinchilion/wwcodechristmascodingchallenge2023/23 - 24 Continuing with Coding for a list app.kt
@@ -1,13 +1,12 @@
 package com.kurinchilion.wwcodechristmascodingchallenge2023
 
-package com.kurinchilion.superheroesapp
-
 import android.media.Image
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kurinchilion.superheroesapp.model.Hero
@@ -24,8 +22,10 @@ import com.kurinchilion.superheroesapp.model.Hero
 @Composable
 private fun SuperHeroItem(hero: Hero, modifier: Modifier = Modifier){
     Card {
-        Row {
-            Column(modifier = modifier.height(72.dp)) {
+        Row (
+            modifier = modifier.height(72.dp)
+                .padding(16.dp)){
+            Column {
                 Text(
                     text = stringResource(hero.nameRes),
                     style = MaterialTheme.typography.displaySmall
@@ -51,7 +51,17 @@ private fun SuperHeroItem(hero: Hero, modifier: Modifier = Modifier){
 }
 
 @Composable
-private fun HeroesList(){
+private fun HeroesList(heroesList: List<Hero>, modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier) {
+        //A call to the items() method requires a lambda function. In that function, specify a parameter
+        // of affirmation that represents one affirmation item from the affirmationList.
+        items(heroesList) { hero ->
+            SuperHeroItem(
+                hero = hero,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
 
 }
 
